1646583f6e2b84705867ce88ff02be9dd37feee5
Srijith Kartha <ksrijith@gmail.com>
2018-05-23 07:24:39 +0000

    Initial Boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// entry point is
+// final bundle js file
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader'
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: "3300",
+        host: "0.0.0.0"
+    }
+};
+
